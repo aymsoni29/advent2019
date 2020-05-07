003db4b2fd651e2f63539a2759f1de762b537c74
Abhay Soni <aymsoni29@gmail.com>
2020-05-07 16:46:16 -0700

    move everything into packages

---
+++ asoni/pkg/day6/orbit.go
@@ -0,0 +1,11 @@
+package day6
+
+import (
+	"fmt"
+	
+	"https://github.com/sean-hart/advent2019/asoni/util"
+)
+
+func main(){
+	fmt.Println("Day 6")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/advent2019
+
+go 1.14
